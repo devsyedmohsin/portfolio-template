f693e73e60407d620c783d3af84f8ac3176367c7
devsyedmohsin <devsyedmohsin@gmail.com>
2023-06-10 12:37:56 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,77 @@
+# Portfolio Template
+
+⚡️ A user-friendly, responsive and accessible portfolio template for developers. [Visit site &rarr;](https://oss-portfolio.netlify.app/) 
+
+Please, give it a star if you find it useful ⭐
+
+
+<img src="assets/images/template.gif.part" alt="lighthouse report with 100% scores">
+
+
+## Table of Contents
+
+1. [Features](#key-features)
+1. [Customization](#customizing-the-template)
+1. [Deploying the Site](#deploying-the-site)
+
+## Features
+
+-  Fully Mobile responsive.
+-  Supports light/dark theme.
+-  Built with a11y in mind.
+-  Modern design.
+-  Multiple sections.
+-  Sleek animations.
+-  Fast loads.
+-  SEO-optimized.
+
+<img src="assets/images/lighthouse.png" alt="lighthouse report with 100% scores">
+
+## Customizing the Template
+
+Follow these two steps to fashion an awe-inspiring portfolio of your own. 
+
+- [Cloning the repository](#cloning-the-repository)
+- [Editing the Template](#editing-the-template)
+- [Deploying the Site](#editing-the-template)
+
+
+### Cloning the Repository.
+
+If you have [Git](https://git-scm.com/) installed on your computer, you can simply clone or download the portfolio by running the following command in your terminal.
+
+```bash
+git clone https://github.com/devsyedmohsin/portfolio-template
+```
+ Alternatively, click on this [link]() to download it. 
+
+### Editing the Template 
+
+After you got the template please open it in the code editor of your choice.
+You will find file named `index.html` open it and follow the guidance inscribed within the comments.
+
+**Note:** While editing the template make sure you have updated all href, img src and alt attributes of links and images. 
+
+**For example:**
+
+```html
+ <!-- Update with your name -->
+<title>Syed Mohsin's Portfolio</title>
+```
+
+```html
+<div class="header-textbox">
+  <!-- Replace with your name & job title -->
+  <h1 class="h1">
+    Hi, I'm Syed Mohsin
+    Frontend Web Developer
+  </h1>
+
+  <!-- Add a small introductory paragraph about yourself -->
+  <p class="header-text">
+    A frontend software engineer and interface designer
+  </p>
+</div>
+```
+
+### Deploying the Site
---
+++ script.js
@@ -0,0 +1,122 @@
+const nav = document.querySelector(".nav");
+const navMenu = document.querySelector(".nav-items");
+const btnToggleNav = document.querySelector(".menu-btn");
+const workEls = document.querySelectorAll(".work-box");
+const workImgs = document.querySelectorAll(".work-img");
+const mainEl = document.querySelector("main");
+
+const toggleNav = () => {
+  nav.classList.toggle("hidden");
+
+  // Prevent screen from scrolling when menu is opened
+  document.body.classList.toggle("lock-screen");
+
+  if (nav.classList.contains("hidden")) {
+    btnToggleNav.textContent = "menu";
+  } else {
+    // When menu is opened after transition change text respectively
+    setTimeout(() => {
+      btnToggleNav.textContent = "close";
+    }, 475);
+  }
+};
+
+btnToggleNav.addEventListener("click", toggleNav);
+
+navMenu.addEventListener("click", (e) => {
+  if (e.target.localName === "a") {
+    toggleNav();
+  }
+});
+
+document.body.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && !nav.classList.contains("hidden")) {
+    toggleNav();
+  }
+});
+
+// Animating work instances on scroll
+
+workImgs.forEach((workImg) => workImg.classList.add("transform"));
+
+let observer = new IntersectionObserver(
+  (entries) => {
+    const [entry] = entries;
+    const [textbox, picture] = Array.from(entry.target.children);
+    if (entry.isIntersecting) {
+      picture.classList.remove("transform");
+      Array.from(textbox.children).forEach(
+        (el) => (el.style.animationPlayState = "running")
+      );
+    }
+  },
+  { threshold: 0.3 }
+);
+
+workEls.forEach((workEl) => {
+  observer.observe(workEl);
+});
+
+// Shiny hover effect on cards that follows mouse
+
+const cards = document.querySelector(".cards");
+
+cards.addEventListener("mousemove", (e) => {
+  if (e.target.classList.contains("card")) {
+    const { x, y } = e.target.getBoundingClientRect();
+    e.target.style.setProperty("--x", `${e.clientX - x}px`);
+    e.target.style.setProperty("--y", `${e.clientY - y}px`);
+  }
+});
+
+// Toggle theme and store user preferred theme for future
+
+const switchThemeEl = document.querySelector('input[type="checkbox"]');
+const storedTheme = localStorage.getItem('theme')
+
+switchThemeEl.checked = storedTheme === 'dark' || storedTheme === null
+
+switchThemeEl.addEventListener("click", () => {
+  const isChecked = switchThemeEl.checked;
+
+  if (!isChecked) {
+    document.body.classList.remove("dark");
+    document.body.classList.add("light");
+    localStorage.setItem("theme", "light");
+    switchThemeEl.checked = false
+  } else {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+    localStorage.setItem("theme", "dark");
+  }
+
+});
+
+// Trap the tab when menu is opened
+
+const lastFocusedEl = document.querySelector('a[aria-data="last-focused"]');
+
+document.body.addEventListener("keydown", (e) => {
+  if (e.key === "Tab" && document.activeElement === lastFocusedEl) {
+    e.preventDefault();
+    btnToggleNav.focus();
+  }
+});
+
+const logosWrappers = document.querySelectorAll(".logo-group");
+
+const sleep = (number) => new Promise((res) => setTimeout(res, number));
+
+logosWrappers.forEach(async (logoWrapper, i) => {
+  const logos = Array.from(logoWrapper.children);
+  await sleep(1400 * i);
+  setInterval(() => {
+    let temp = logos[0];
+    logos[0] = logos[1];
+    logos[1] = logos[2];
+    logos[2] = temp;
+    logos[0].classList.add("hide", "to-top");
+    logos[1].classList.remove("hide", "to-top", "to-bottom");
+    logos[2].classList.add("hide", "to-bottom");
+  }, 5600);
+});
